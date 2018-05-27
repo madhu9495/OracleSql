e90c1ad4056003b07bba0961d832db069f8f8edc
madhu9495 <rmadhu9495@gmail.com>
2018-05-27 19:03:03 +0530

    Update Exercises.sql

--- Exercises.sql
+++ Exercises.sql
@@ -1,3 +1,5 @@
 select last_name || ',' || ' ' || job_id "Employee and Title" from HR.EMPLOYEES;
 
 select EMPLOYEE_ID || ',' || FIRST_NAME || ',' || LAST_NAME || ',' || EMAIL || ',' || PHONE_NUMBER || ',' || HIRE_DATE || ',' || JOB_ID  ||',' || SALARY ||',' || COMMISSION_PCT ||',' || MANAGER_ID ||',' || DEPARTMENT_ID "Employee" from HR.EMPLOYEES;
+
+select last_name, salary from HR.EMPLOYEES where salary not between 5000 and 12000;
